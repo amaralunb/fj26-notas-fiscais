84348ab71b1ad9959055d00e8bc89d9622bd6987
amaralunb <lgaa2006@gmail.com>
2015-04-22 10:28:42 -0300

    Basic files of Notas Fiscais' system

---
+++ fj-26/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -0,0 +1,21 @@
+package br.com.caelum.notasfiscais.mb;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Produto;
+
+@ManagedBean
+public class ProdutoBean {
+	private Produto produto = new Produto();
+
+	public Produto getProduto() {
+		return this.produto;
+	}
+	
+	public void adiciona () {
+		ProdutoDao dao = new ProdutoDao();
+		dao.adiciona(produto);
+	}
+	
+}
