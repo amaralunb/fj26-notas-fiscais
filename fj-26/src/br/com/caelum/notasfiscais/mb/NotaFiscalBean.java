d19af3f1483dde1e4fcb80bd39b12c27cae4590c
amaralunb <lgaa2006@gmail.com>
2015-04-27 13:21:38 -0300

    Creation of logic and webpage of our invoice register

--- fj-26/src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
+++ fj-26/src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
@@ -5,12 +5,20 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.NotaFiscalDao;
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Item;
 import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+import br.com.caelum.notasfiscais.modelo.Produto;
 
 @Named
 @RequestScoped
 public class NotaFiscalBean {
 	private NotaFiscal notaFiscal = new NotaFiscal();
+	private Item item = new Item();
+	private Long idProduto;
+	
+	@Inject
+	private ProdutoDao produtoDao;
 	
 	@Inject
 	private NotaFiscalDao nfDao;
@@ -21,7 +29,34 @@ public class NotaFiscalBean {
 		this.notaFiscal = new NotaFiscal();
 	}
 	
+	public void guardaItem() {
+		Produto produto = produtoDao.buscaPorId(idProduto);
+		item.setProduto(produto);
+		item.setValorUnitario(produto.getPreco());
+		
+		notaFiscal.getItens().add(item);
+		item.setNotaFiscal(notaFiscal);
+		
+		item = new Item();
+	}
+	
 	public NotaFiscal getNotaFiscal() {
 		return notaFiscal;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
 	}
 }
