aad666495d2d0e36e2144ca2e703f1eb22ea08ba
amaralunb <lgaa2006@gmail.com>
2015-04-29 13:44:58 -0300

    Creation of a invoice list

---
+++ fj-26/src/br/com/caelum/notasfiscais/mb/ListaNotasFiscaisBean.java
@@ -0,0 +1,28 @@
+package br.com.caelum.notasfiscais.mb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.com.caelum.notasfiscais.dao.Dao;
+import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+import br.com.caelum.notasfiscais.stereotypes.ViewModel;
+
+@ViewModel
+public class ListaNotasFiscaisBean {
+	private List<NotaFiscal> nfLista = new ArrayList<NotaFiscal>();
+	
+	@Inject
+	private Dao<NotaFiscal> nfDao;
+	
+	public List<NotaFiscal> getNfLista() {
+		return nfLista;
+	}
+
+	public void setNfLista(List<NotaFiscal> nfLista) {
+		this.nfLista = nfLista;
+	}
+
+	
+}
